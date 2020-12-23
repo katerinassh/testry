6e5a4adb4bb3961b9f7c99399607c9ca97d95e79
Katerina <68004503+katerinassh@users.noreply.github.com>
2020-12-24 01:58:51 +0200

    редактирование формы для редактирование и добавление предупрждений

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from PyQt5 import QtWidgets
-from creator import Ui_MainWindow
-from create_test import Form2
-from edit_test import Form3
-from delete_test import Form4
-import sys
+from creator_design import Ui_MainWindow
+from create_test_design import Form2
+from edit_test_design import Form3
+
+import delete_test
+import warnings
 import test
-import os
+import sys
 
 
-class MainWindow(QtWidgets.QMainWindow):
+
+class MainWindow(QtWidgets.QMainWindow): # главный экран программы
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.current_test = None
+        self.title = None
         self.feedback = None
 
         self.lineEdit = self.findChild(QtWidgets.QLineEdit, 'lineEdit')
@@ -26,32 +29,55 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.pushButton_2.clicked.connect(self.pushButton_2_clicked)
 
-    def pushButton_2_clicked(self):
+    def pushButton_2_clicked(self): # выбор действия с creator mode
         if self.radioButton.isChecked():
             self.pushButton_2.clicked.connect(self.window2)
+
         elif self.radioButton_3.isChecked():
-            self.pushButton_2.clicked.connect(self.window3)
+            if self.lineEdit.text() == '':
+                self.warning1()
+            else:
+                self.pushButton_2.clicked.connect(self.window3)
+
         elif self.radioButton_2.isChecked():
-            self.pushButton_2.clicked.connect(self.window4)
+            if self.lineEdit.text() == '':
+                self.warning1()
+            else:
+                self.pushButton_2.clicked.connect(self.window4)
+
+    def push_error(self):
+        self.warning2()
 
     def main_window(self):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.show()
 
-    def window2(self):
+    def window2(self): # показывает окно для создания теста
         self.w2 = CreateWindow(self.lineEdit.text())
         self.w2.show()
         self.hide()
 
-    def window3(self):
+    def window3(self): # показывает окно для редактирования теста
         self.w3 = EditWindow(self.lineEdit.text())
+        if FileNotFoundError:
+            self.warning2()
         self.w3.show()
         self.hide()
 
-    def window4(self):
-        self.w4 = DeleteWindow(self.lineEdit.text())
+    def window4(self): # показывает окно для удаления теста
+        self.w4 = delete_test.DeleteWindow(self.lineEdit.text())
+        if FileNotFoundError:
+            self.warning2()
         self.w4.show()
+
+    def warning1(self): # показывает окно с предупреждением о том, что назввание теста было не написано
+        self.war = warnings.WarningWindow1()
+        self.war.show()
+
+    def warning2(self): # показывает окно с предупреждением о том, что такого теста не существует
+        self.war = warnings.WarningWindow2()
+        self.war.show()
 
 
 class CreateWindow(QtWidgets.QMainWindow):
@@ -93,13 +119,23 @@ class EditWindow(QtWidgets.QMainWindow):
         self.ui = Form3()
         self.ui.setupUi(self)
         self.title = title
+        self.current_test = None
 
         self.label_2 = self.findChild(QtWidgets.QLabel, 'label_2')
         self.label_2.setText(self.title)
+        self.label_3 = self.findChild(QtWidgets.QLabel, 'label_3')
+
         self.pushButton_4 = self.findChild(QtWidgets.QPushButton, 'pushButton_4')
         self.pushButton_4.clicked.connect(self.window1)
         self.pushButton_5 = self.findChild(QtWidgets.QPushButton, 'pushButton_5')
         self.pushButton_5.clicked.connect(self.window4)
+        self.loadFile()
+
+    def loadFile(self): # загружает на форму тест из файла
+        file = open('{}.txt'.format(self.title), 'r')
+        with file:
+            data = file.read()
+        self.label_3.setText(data)
 
     def window1(self):
         self.w1 = MainWindow()
@@ -107,29 +143,9 @@ class EditWindow(QtWidgets.QMainWindow):
         self.hide()
 
     def window4(self):
-        self.w4 = DeleteWindow(self.title)
+        self.w4 = delete_test.DeleteWindow(self.title)
         self.w4.show()
 
-
-class DeleteWindow(QtWidgets.QMainWindow):
-    def __init__(self, title):
-        super(DeleteWindow, self).__init__()
-        self.ui = Form4()
-        self.ui.setupUi(self)
-        self.title = title
-
-        self.label_2 = self.findChild(QtWidgets.QLabel, 'label_2')
-        self.label_2.setText(self.title)
-        self.label_3 = self.findChild(QtWidgets.QLabel, 'label_3')
-        self.pushButton_3 = self.findChild(QtWidgets.QPushButton, 'pushButton_3')
-        self.pushButton_3.clicked.connect(self.delete)
-        self.pushButton_4 = self.findChild(QtWidgets.QPushButton, 'pushButton_4')
-        self.pushButton_4.clicked.connect(self.close)
-
-    def delete(self): # удаляет тест
-        os.remove('{}.txt'.format(self.title))
-        os.remove('{}_answers.txt'.format(self.title))
-        self.label_3.setText('Deleted!')
 
 
 app = QtWidgets.QApplication([])
